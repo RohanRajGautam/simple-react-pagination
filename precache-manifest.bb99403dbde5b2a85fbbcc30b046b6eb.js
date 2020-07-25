6c406c5a0b06a1a131634b8f06edfd9c6c379b3f
rohan <rohanrajgautam@gmail.com>
2020-07-25 09:54:57 +0545

    Updates

---
+++ precache-manifest.bb99403dbde5b2a85fbbcc30b046b6eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b9e8656d161525cebf6ace0496277c4",
+    "url": "/simple-react-pagination/index.html"
+  },
+  {
+    "revision": "873c14b11b65020c928e",
+    "url": "/simple-react-pagination/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "0ee7a988d29584e043fb",
+    "url": "/simple-react-pagination/static/js/2.4fddb86c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-react-pagination/static/js/2.4fddb86c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "873c14b11b65020c928e",
+    "url": "/simple-react-pagination/static/js/main.4d9b33d8.chunk.js"
+  },
+  {
+    "revision": "398a525f3c06a9bb044b",
+    "url": "/simple-react-pagination/static/js/runtime-main.363f827f.js"
+  }
+]);
